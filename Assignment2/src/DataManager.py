c6690c5af49cda0c7e203ac1cd05c097b4d5af5f
Adam Kohl <herbert@HERBERT.local>
2018-01-25 15:54:32 -0600

    Removing unnecessary functions within DataManager and main

--- Assignment2/src/DataManager.py
+++ Assignment2/src/DataManager.py
@@ -1,7 +1,6 @@
 from DataImporter import DataImporter
 import statistics as st
 import scipy.stats as scipstat
-import math
 
 class DataManager:
 
@@ -10,55 +9,6 @@ class DataManager:
         self.import_path = import_file_path
         self.output_path = output_file_path
         self.dataImport = DataImporter()
-
-        # Variables specific to application
-        self.energy_variables = []
-        self.energy_data = {}
-        self.airfoil_data = {}
-        self.airfoil_stats = {}
-        self.airfoil_var_names = ['Frequency (Hz)', 'Angle of Attack (degrees)', 'Chord Length (m)', 'Free-stream Velocity (m/s)', 'Suction Side Displacement Thickness (m)']
-
-    def importEnergyData(self, fileName):
-        # Import data from file
-        file_name = self.import_path + fileName
-        raw_data = self.dataImport.readCSVFile(file_name)
-
-        # Sort and convert data
-        self.energy_variables = raw_data[1]
-        point_id = 1
-        for key, value in raw_data.items():
-            if key > 1:
-                data_point = []
-                for i in range(len(value)):
-                    if i == 0:
-                        # Dates to be stored as string
-                        data_point.append(value[i])
-                    else:
-                        # Variable values to be stored float
-                        data_point.append(float(value[i]))
-
-                # Store data point array in dictionary
-                self.energy_data[point_id] = data_point
-                point_id += 1
-
-    def importAirfoilData(self, fileName):
-        # Import data from file
-        file_name = self.import_path + fileName
-        raw_data = self.dataImport.readDATFile(file_name)
-
-        # Sort and convert data
-        point_id = 1
-        for key, value in raw_data.items():
-            data_point = []
-            for point in value:
-                # Variable values to be stored float
-                data_point.append(float(point))
-
-            # Store data point array in dictionary
-            self.airfoil_data[point_id] = data_point
-            point_id += 1
-
-        self.airfoil_stats = self.computeGeneralStatistics(self.airfoil_data)
 
     def computeGeneralStatistics(self, dataDict):
         # Initialize variable and statistic dictionary
@@ -74,7 +24,7 @@ class DataManager:
                 column_data[i].append(value[i])
 
         # Stats array contains - [mean, std, median, kurtosis, skewness, range[min, max]]
-        varID = 0
+        var_id = 0
         for key, value in column_data.items():
             stats_dict[0].append(st.mean(value))
             stats_dict[1].append(st.stdev(value))
@@ -82,13 +32,5 @@ class DataManager:
             stats_dict[3].append(scipstat.kurtosis(value))
             stats_dict[4].append(scipstat.skew(value))
             stats_dict[5].append([min(value), max(value)])
-            #
-            # print("Variable:\t" + self.airfoil_var_names[varID])
-            # print("Mean: \t" + st.mean(value))
-            # print("Std: \t" + st.stdev(value))
-            # print("Median: \t" +st.median(value))
-            # print("Kurtosis: \t" + scipstat.kurtosis(value))
-            # print("Skewness: \t" + scipstat.skew(value))
-            # print("Range: \t" + min(value) + "," + max(value))
-            varID += 1
+            var_id += 1
         return stats_dict
--- Assignment2/src/main.py
+++ Assignment2/src/main.py
@@ -1,48 +1,12 @@
-from DataManager import DataManager
-from TimeSeriesProcessor import TimeSeriesProcessor
+
 
 DATA_DIR = "/Users/herbert/Code/ME592X/Assignment_1/data/"
 OUTPUT_DIR = "/Users/herbert/Code/ME592X/Assignment_1/output/"
 
 
 def main():
-    # --------------- Problem 2.2 -------------------------------
-    print("Starting Assignment 1 Application Execution")
-    print("Executing Problem 2.2")
-
-    # Import data
-    dataMan = DataManager(DATA_DIR, OUTPUT_DIR)
-    dataMan.importEnergyData("energydata_complete.csv")
-
-    # Store and process time-series data
-    tseriesProcesser = TimeSeriesProcessor()
-    tseriesProcesser.setData(dataMan.energy_data)
-    tseriesProcesser.setVariableInfo(dataMan.energy_variables)
-
-    # --------------- Problem 2.2.1 -----------------------------
-    print("Executing Problem 2.2.1")
-    tseriesProcesser.analyzeEntireTimePeriod()
-    tseriesProcesser.analyzeWeek()
-
-    # --------------- Problem 2.2.2 & 2.2.3 ---------------------
-    print("Executing Problem 2.2.2 and 2.2.3")
-    tseriesProcesser.analyzeEnergyConsumption()
-
-    # --------------- Problem 2.2.4 -----------------------------
-    print("Executing Problem 2.2.4")
-
-    # --------------- Problem 2.2.5 -----------------------------
-    print("Executing Problem 2.2.5")
-    # tseriesProcesser.analyzePressureImpact()
-
-    # --------------- Problem 2.2.6 -----------------------------
-    print("Executing Problem 2.2.6")
-
-    # --------------- Problem 2.3 -------------------------------
-    print("Executing Problem 2.3")
-    # Import data
-    dataMan.importAirfoilData("airfoil_self_noise.dat")
-    print("stop")
+    # --------------- Assignment 2 -------------------------------
+    print("Executing Assignment 2")
 
 
 if __name__ == "__main__":
